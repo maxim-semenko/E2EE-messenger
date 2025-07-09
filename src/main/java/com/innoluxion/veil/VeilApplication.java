c75c7517d3dd95d4ae7f24535a09b12c45fd10dd
maxim-semenko <maks.semenko@gmail.com>
2025-07-09 13:35:29 +0400

    create and init project

---
+++ src/main/java/com/innoluxion/veil/VeilApplication.java
@@ -0,0 +1,13 @@
+package com.innoluxion.veil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innoluxion/veil/VeilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innoluxion.veil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
