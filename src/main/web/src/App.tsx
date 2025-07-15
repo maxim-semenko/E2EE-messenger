c2d4fd932d2969036cec7acc390b3f06d1cc53ee
maxim-semenko <maks.semenko@gmail.com>
2025-07-16 01:40:01 +0400

    other changes

--- src/main/web/src/App.tsx
+++ src/main/web/src/App.tsx
@@ -1,50 +1,50 @@
-import {useEffect, useState} from 'react'
 import './App.css'
-import {exportCryptoKeyToPEM, generateRSAKeyPair} from "./lib/keys.ts";
 import WebSocketContext from "./context/WebSocketContext.ts";
 import useWebSocket from "./hooks/useWebSocket.ts";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HomePage from "./components/HomePage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import {Provider} from "@/components/ui/provider"
+import UserContext, {type User} from "@/context/UserContext.ts";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [isGenerated, setIsGenerated] = useState(false)
 
+    const [user, setUser] = useState<User | null>(null);
     useEffect(() => {
-        if (!isGenerated) {
-            generateRSAKeyPair().then(async keys => {
-                console.log('Public Key:', keys.publicKey);
-                console.log('Private Key:', keys.privateKey);
-                const [publicPem, privatePem] = await Promise.all([exportCryptoKeyToPEM(keys.publicKey), exportCryptoKeyToPEM(keys.privateKey!)]);
+        const userId = localStorage.getItem("userId");
+        const privateKey = localStorage.getItem("privateKey");
+        const publicKey = localStorage.getItem("publicKey");
 
-                console.log("Public Key:\n", publicPem);
-                console.log("Private Key:\n", privatePem);
-                setIsGenerated(true);
-            });
+        const user: User = {
+            id: userId || "",
+            privateKey: privateKey || "",
+            publicKey: publicKey || ""
         }
-
+        setUser(user)
     }, []);
 
     const {ws} = useWebSocket();
 
-    if (!isGenerated) {
-        return <h1>Loading...</h1>
-    }
 
     return (
-        <WebSocketContext.Provider value={ws}>
-            <BrowserRouter>
-                {/*<nav style={{display: 'flex', gap: '1rem'}}>*/}
-                {/*    <Link to="/">Home</Link>*/}
-                {/*    <Link to="/about">About</Link>*/}
-                {/*</nav>*/}
+        <Provider>
+            <UserContext.Provider value={{user, setUser}}>
+                <WebSocketContext.Provider value={ws}>
+                    <BrowserRouter>
+                        {/*<nav style={{display: 'flex', gap: '1rem'}}>*/}
+                        {/*    <Link to="/">Home</Link>*/}
+                        {/*    <Link to="/about">About</Link>*/}
+                        {/*</nav>*/}
 
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    {/*<Route path="/about" element={<AboutPage/>}/>*/}
-                    {/*<Route path="*" element={<NotFoundPage />} />*/}
-                </Routes>
-            </BrowserRouter>
-        </WebSocketContext.Provider>
+                        <Routes>
+                            <Route path="/" element={<HomePage/>}/>
+                            {/*<Route path="/about" element={<AboutPage/>}/>*/}
+                            {/*<Route path="*" element={<NotFoundPage />} />*/}
+                        </Routes>
+                    </BrowserRouter>
+                </WebSocketContext.Provider>
+            </UserContext.Provider>
+        </Provider>
     )
 }
 
--- src/main/web/src/main.tsx
+++ src/main/web/src/main.tsx
@@ -2,6 +2,10 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App/>
