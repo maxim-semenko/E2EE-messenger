c12388630c571be9d0765b03a88e2fa7a816a564
maxim-semenko <maks.semenko@gmail.com>
2025-07-17 18:49:05 +0400

    removed UserContext

--- src/main/web/src/App.tsx
+++ src/main/web/src/App.tsx
@@ -3,24 +3,19 @@ import WebSocketContext from "./context/WebSocketContext.ts";
 import useWebSocket from "./hooks/useWebSocket.ts";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "@/components/ui/provider"
-import UserContext, {type User} from "@/context/UserContext.ts";
-import {useState} from "react";
 import AppRoutes from "@/pages/AppRouters.tsx";
 
 
 function App() {
-    const [user, setUser] = useState<User | null>(null);
     const {ws} = useWebSocket();
 
     return (
         <Provider>
-            <UserContext.Provider value={{user, setUser}}>
-                <WebSocketContext.Provider value={ws}>
-                    <BrowserRouter>
-                        <AppRoutes/>
-                    </BrowserRouter>
-                </WebSocketContext.Provider>
-            </UserContext.Provider>
+            <WebSocketContext.Provider value={ws}>
+                <BrowserRouter>
+                    <AppRoutes/>
+                </BrowserRouter>
+            </WebSocketContext.Provider>
         </Provider>
     );
 }
