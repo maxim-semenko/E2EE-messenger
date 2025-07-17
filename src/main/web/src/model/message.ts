42d96d78b8cf1ebc7718a3beb2f1858c2068d1a6
maxim-semenko <maks.semenko@gmail.com>
2025-07-17 18:48:51 +0400

    added models

---
+++ src/main/web/src/model/message.ts
@@ -0,0 +1,20 @@
+export enum MessageType {
+    CHAT = 'CHAT',
+    HELLO = 'HELLO',
+    IAM = 'IAM',
+    CONTENT = 'CONTENT',
+    WHO = 'WHO',
+    SERVER = 'SERVER',
+}
+
+export interface Message {
+    id?: string;
+    sender: string;
+    receiver: string;
+    chat: string;
+    type: MessageType;
+    content?: string;
+    iv: string,
+    created?: Date;
+    decrypted?: boolean
+}
